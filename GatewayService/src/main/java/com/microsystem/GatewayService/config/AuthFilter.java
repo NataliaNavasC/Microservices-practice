2a793dbf7d2e541d98b8193f93d1f73b389d795a
BeltranDaniel <beltran_daniel@javeriana.edu.co>
2021-09-23 17:40:34 -0500

    Created user service and added gateway configuration

---
+++ GatewayService/src/main/java/com/microsystem/GatewayService/config/AuthFilter.java
@@ -0,0 +1,49 @@
+package com.microsystem.GatewayService.config;
+
+import com.google.common.net.HttpHeaders;
+import com.microsystem.GatewayService.model.UserDto;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+    
+    private final WebClient.Builder webClientBuilder;
+
+    public AuthFilter(WebClient.Builder webClientBuilder) {
+        super(Config.class);
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                throw new RuntimeException("Missing authorization information");
+            }
+            String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+            String[] parts = authHeader.split(" ");
+
+            if (parts.length != 2 || !"Bearer".equals(parts[0])) {
+                throw new RuntimeException("Incorrect authorization structure");
+            }
+
+            return webClientBuilder.build()
+                    .post()
+                    .uri("http://service-users/users/validateToken?token=" + parts[1])
+                    .retrieve().bodyToMono(UserDto.class)
+                    .map(userDto -> {
+                        exchange.getRequest()
+                                .mutate()
+                                .header("X-auth-user-id", String.valueOf(userDto.getId()));
+                        return exchange;
+                    }).flatMap(chain::filter);
+        };
+    }
+
+    public static class Config {
+        // empty class as I don't need any particular configuration
+    }
+}
