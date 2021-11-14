96f5b9bcd9d638becdf6711207fd526b61e76932
BeltranDaniel <beltran_daniel@javeriana.edu.co>
2021-11-14 13:21:05 -0500

    updated search

--- MicroServicios/TourismServicesService/src/main/java/com/microsystem/tourismServicesService/Utils/Constans.java
+++ MicroServicios/TourismServicesService/src/main/java/com/microsystem/tourismServicesService/Utils/Constans.java
@@ -1,6 +1,6 @@
 package com.microsystem.tourismServicesService.Utils;
 
 public class Constans {
-    public static String SearchSoapURL = "http://localhost:2220/ws";
+    public static String SearchSoapURL = "http://localhost:8083/search/ws";
     public static String SearchSoapClassPackage = "com.microsystem.tourismServicesService.stub";
 }
