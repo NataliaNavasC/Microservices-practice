138cd954c5d3a16e4d825160813793210ea8038a
BeltranDaniel <beltran_daniel@javeriana.edu.co>
2021-11-13 22:07:35 -0500

    update wsdl

--- MicroServicios/TourismServicesService/src/main/java/com/microsystem/tourismServicesService/Utils/Constans.java
+++ MicroServicios/TourismServicesService/src/main/java/com/microsystem/tourismServicesService/Utils/Constans.java
@@ -1,6 +1,6 @@
 package com.microsystem.tourismServicesService.Utils;
 
 public class Constans {
-    public static String SearchSoapURL = "http://localhost:8080/search/ws";
+    public static String SearchSoapURL = "http://localhost:8084/search/ws";
     public static String SearchSoapClassPackage = "com.microsystem.tourismServicesService.wsdl";
 }
